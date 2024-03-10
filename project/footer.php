0bb2b19f6e3e65209bb40cc35874cad346f32b28
Anass Braim <146454518+anassbraim@users.noreply.github.com>
2024-03-10 17:38:55 +0100

    Add files via upload

---
+++ project/footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>Quiks links</h3>
+         <a href="home.php">Home</a>
+         <a href="about.php">About</a>
+         <a href="shop.php">Shop</a>
+         <a href="contact.php">Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra Links</h3>
+         <a href="login.php">Login</a>
+         <a href="register.php">Register</a>
+         <a href="cart.php">Cart</a>
+         <a href="orders.php">Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +212 84 71 77 96</p>
+         <p> <i class="fas fa-phone"></i> +212 84 71 77 96</p>
+         <p> <i class="fas fa-envelope"></i>anassbraim7@gmail.com</p>
+         <p> <i class="fas fa-map-marker-alt"></i>Agadir, Morocco</p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i>Facebook</a>
+         <a href="#"> <i class="fab fa-twitter"></i>X</a>
+         <a href="#"> <i class="fab fa-instagram"></i>Instagram</a>
+         <a href="#"> <i class="fab fa-whatsapp"></i>WhatsApp</a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy;All copyrights reserved <?php echo date('Y'); ?><span> Anass Braim</span> </p>
+
+</section>
